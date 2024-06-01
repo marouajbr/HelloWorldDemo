eee92c2407b729f7f05f3329be78847f6d49773a
DELL <marwa.jabroun.sordo@gmail.com>
2024-06-01 23:15:22 +0100

    first version

---
+++ src/main/java/com/learning/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.learning.demo;
+
+/**
+ * @Author : Maroua jbr
+ * @Deccription : This is java project
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
